ca32e46929fa3f8f6a057a2e1e8531504d00bb7b
Vladimir G. <vglazunov@renci.org>
2026-07-28 12:04:56 -0400

    Added DELETE and PUT. Added status codes to return

--- Networking/HttpRequest.go
+++ Networking/HttpRequest.go
@@ -8,42 +8,89 @@ import (
 	"net/http"
 )
 
-func HttpGet(url string) ([]byte, error) {
+func HttpGet(url string) (int, []byte, error) {
 	client := GetHttpClientWithNoTLSCheck()
 	resp, err := client.Get(url)
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
-	return body, err
+	return resp.StatusCode, body, err
 }
 
-func HttpPost(url string, body map[string]any) ([]byte, error) {
+func HttpPost(url string, body map[string]any) (int, []byte, error) {
 	client := GetHttpClientWithNoTLSCheck()
 	jsonValue, err := json.Marshal(body)
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
 	resp, err := client.Post(url, "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
-		return nil, err
+		return 0, nil, err
 	}
 
 	defer resp.Body.Close()
 
 	resp_body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return resp.StatusCode, nil, err
 	}
 
-	return resp_body, err
+	return resp.StatusCode, resp_body, err
+}
+
+func HttpDelete(url string) (int, []byte, error) {
+	client := GetHttpClientWithNoTLSCheck()
+
+	req, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return resp.StatusCode, nil, err
+	}
+	return resp.StatusCode, body, nil
+}
+
+func HttpPutWithStatus(url string, body map[string]any) (int, []byte, error) {
+	client := GetHttpClientWithNoTLSCheck()
+	jsonValue, err := json.Marshal(body)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	req, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(jsonValue))
+	if err != nil {
+		return 0, nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, nil, err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return resp.StatusCode, nil, err
+	}
+	return resp.StatusCode, respBody, nil
 }
 
 func GetHttpClientWithNoTLSCheck() *http.Client {
